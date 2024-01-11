d09b8efb5e3e8b30dbfd544385fa867663be63a6
tgriarte <trevorgriarte@gmail.com>
2024-01-11 10:18:49 -0800

    Initial commit

---
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -0,0 +1,77 @@
+package com.bmhs.gdxintro;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class AppHandler extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	int x, y;
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+
+		x = 0;
+		y = 0;
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img,x,y);
+		batch.end();
+		checkInput();
+	}
+
+	public void checkInput(){
+		/*
+		if(Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+			y += 100;
+			// y = y + 10;
+		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.S)) {
+			y -= 100;
+		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+			x -= 100;
+		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.D)) {
+			x += 100;
+		}
+
+
+
+		System.out.println(Gdx.input.getX() + " " + Gdx.input.getY());
+
+		x = Gdx.input.getX() - img.getWidth()/2;
+		y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
+
+		 */
+
+		if(x < (Gdx.input.getX() - img.getWidth()/2)) {
+			x+=2;
+		}
+		if(x > (Gdx.input.getX() - img.getWidth()/2)) {
+			x-=2;
+		}
+		if(y > (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
+			y-=20;
+		}
+		if(y < (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
+			y+=20;
+		}
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
